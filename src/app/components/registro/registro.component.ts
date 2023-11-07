7d07cd13e2100c098009e7b5354752e2b4131522
JaimeGrimaldo <DankerGamer-@hotmail.com>
2023-11-07 16:43:27 -0600

    Enlistar usuarios - listo

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -3,14 +3,12 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+  styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent {
-
-
   mostrarRegistro = true;
   mostrarListar = false;
-  
+
   constructor(private router: Router) {}
   id!: string;
   nombreCompleto!: string;
@@ -26,12 +24,12 @@ export class RegistroComponent {
       nombreCompleto: this.nombreCompleto,
       direccion: this.direccion,
       fechaNacimiento: this.fechaNacimiento,
-      foto: this.foto
+      foto: this.imagenBase64,
+      edad: this.calcularEdad(this.fechaNacimiento)
     };
 
     this.usuarios.push(usuario);
 
-    
     this.id = '';
     this.nombreCompleto = '';
     this.direccion = '';
@@ -46,7 +44,6 @@ export class RegistroComponent {
     console.log(this.usuarios);
   }
 
-
   public irListar() {
     this.mostrarRegistro = false;
     this.mostrarListar = true;
@@ -57,7 +54,32 @@ export class RegistroComponent {
     this.mostrarListar = false;
   }
 
-  public calcularEdad() {
-    
+  calcularEdad(fechaNacimiento: Date): number {
+    const hoy = new Date();
+    const fechaNac = new Date(fechaNacimiento);
+    const edad = hoy.getFullYear() - fechaNac.getFullYear();
+
+    if (
+      hoy < new Date(hoy.getFullYear(), fechaNac.getMonth(), fechaNac.getDate())
+    ) {
+      return edad - 1;
+    }
+
+    return edad;
+  }
+
+  imagenBase64: string | undefined;
+
+  cargarImagen(event: any) {
+    const archivo = event.target.files[0];
+
+    if (archivo) {
+      const lector = new FileReader();
+      lector.onload = (e: any) => {
+        this.imagenBase64 = e.target.result;
+      };
+
+      lector.readAsDataURL(archivo);
+    }
   }
 }
